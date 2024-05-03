5b4dc5b31a59d3ada943f7a349ceba9e16ba50fc
Anahi-Nany <a324947@alumnos.uaslp.mx>
2024-05-03 07:12:57 -0600

    log in with spotify: changing key

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,102 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class SpotifyService {
+    public credentials = {
+
+        clientId: 'c412424ad8c2474c86370c9b52b4c640',
+        clientSecret: 'd2f0d0fb908647afa4a8223b433d119d',
+        accessToken: ''
+    };
+
+    public poolUR1S = {
+        authorize: 'https://accounts.spotify.com/es-ES/authorize?client_id=' +
+            this.credentials.clientId + '&response_type=token' +
+            '&redirect_uri=' + encodeURIComponent('http://localhost:4200/callback') +
+            '&expires_in-3600',
+        refreshAccessToken: 'https://accounts.spotify.com/api/token'
+
+    };
+    constructor(private _http: HttpClient) {
+        this.upDateToken()
+    }
+
+    upDateToken() {
+        this.credentials.accessToken = sessionStorage.getItem('token') || '';
+    }
+
+
+    getQuery(query: string) {
+
+        const URL = "https://api.spotify.com/v1/${query}";
+        const HEADER = { headers: new HttpHeaders({ 'Authorization': 'Bearer' + this.credentials.accessToken }) };
+
+        return this._http.get(URL, HEADER);
+    }
+
+    checkTokenSpoLogin() {
+        //falta text
+        this.checkTokenSpo() || (sessionStorage.setItem('refererURL', location.href), window.location.href = '');
+
+    }
+
+    checkTokenSpo() {
+        return !!this.credentials.accessToken;
+    }
+
+    tokenRefreshURL() {
+
+        this.checkTokenSpo() && alert('Expiro la sesión');
+        this.credentials.accessToken = '';
+        sessionStorage.removeItem('token');
+        this.checkTokenSpoLogin();
+    }
+
+
+    getNewReleases() {
+        return this.getQuery("browse/new-releases")
+            .pipe(map((data: any) => data.albums.items));
+    }
+
+    getFeaturedPlaylists() {
+        return this.getQuery(`browse/featured-playlists`)
+            .pipe(map((data: any) => data.playlists.items));
+    }
+
+    getArtistas(termino: string) {
+
+        return this.getQuery(`search?q=${termino} &type=artist&limit=15`)
+            .pipe(map((data: any) => {
+                return data.artists.items;
+            }));
+    }
+
+    getPlaylist(id: string) {
+        return this.getQuery(`playlists/${id}`)
+    }
+
+    getArtista(id: string) {
+        return this.getQuery(`artist/${id}`)
+    }
+
+    getTopTracks(id: string) {
+        return this.getQuery(`artists/${id}/top-tracks?market=US`)
+            .pipe(map((data: any) => {
+                return data["tracks"];
+            }));
+    }
+
+    getTopPlaylistTracks(id: string) {
+        return this.getQuery(`playlists/${id}/tracks&limit=15`)
+            .pipe(map((data: any) => {
+                return data["tracks"];
+            }));
+    }
+
+}
