c34d73fa5f8668bcc6e6534fdb4f554a80ce690b
Anahi-Nany <a324947@alumnos.uaslp.mx>
2024-05-27 10:29:06 -0600

    Hide Navbar in Welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { filter } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ilovspoty';
+
+  showHeader: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        if (event instanceof NavigationEnd) {
+          this.showHeader = event.urlAfterRedirects !== '/welcome';
+        }
+      });
+  }
 }
